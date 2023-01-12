31e227488540dc0bf35e2a0822b314454647c7ba
Yamauchi <yama@naosan.jp>
2023-01-12 15:45:47 +0900

    Series #1, #2, #3 initial upload

---
+++ dotnet/1/GrpcHelloClient/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcHelloClient;
+
+//using var channel = GrpcChannel.ForAddress("https://localhost:7231");
+using var channel = GrpcChannel.ForAddress("http://localhost:5289");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "やまうちなお" });
+Console.WriteLine("gRPC: " + reply.Message);
